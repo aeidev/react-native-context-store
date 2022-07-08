175fb35b3d95b1121308257b62aa637e4ae8d3b1
andrew e <frostie.dev@gmail.com>
2022-07-08 14:04:41 -0700

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  ContextStoreExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-context-store': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
